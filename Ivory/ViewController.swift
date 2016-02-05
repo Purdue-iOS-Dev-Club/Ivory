4407d98788e765c336d09c86fa7f67b89873b357
George Lo <twgeolo@gmail.com>
2016-02-05 13:44:07 -0500

    Initial Commit

---
+++ Ivory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ivory
+//
+//  Created by George Lo on 2/5/16.
+//  Copyright © 2016 Purdue iOS Dev Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
